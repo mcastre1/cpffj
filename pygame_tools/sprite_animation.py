dd5481796a97c1a0708dab3dda9f5ded6b2defdd
mcastre1 <miguelcastrejongal@gmail.com>
2023-10-22 12:49:47 -0600

    Finished succesfuly loading sprites

--- pygame_tools/sprite_animation.py
+++ pygame_tools/sprite_animation.py
@@ -4,18 +4,23 @@ import sys
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, image_path, tile_size):
         super().__init__()
-        self.images = self.split_image(image_path)
         self.tile_size = tile_size
+        self.images = self.split_image(image_path)
 
-        print(self.images)
+        self.sprite_index = 0
+        self.sprite_speed = 0.1
+
+        self.image = pygame.Surface((tile_size, tile_size))
+        self.rect = self.image.get_rect()
+        self.image.fill('white')
 
 
     def split_image(self, path):
         images = []
 
         image = pygame.image.load(path).convert_alpha()
-        x_tiles = image.get_width()/self.tile_size
-        y_tiles = image.get_height()/self.tile_size
+        x_tiles = int(image.get_width()/self.tile_size)
+        y_tiles = int(image.get_height()/self.tile_size)
 
         for row in range(y_tiles):
             for col in range(x_tiles):
@@ -27,6 +32,18 @@ class Sprite(pygame.sprite.Sprite):
                 images.append(new_surface)
 
         return images
+    
+    def animate(self):
+        self.sprite_index += self.sprite_speed
+
+        if self.sprite_index >= len(self.images):
+            self.sprite_index = 0
+
+        self.image = self.images[int(self.sprite_index)]
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.animate()
 
 
 
@@ -37,9 +54,9 @@ class Sprite_animator():
         self.display_surface = pygame.display.set_mode((500,500))
         self.clock = pygame.time.Clock()
 
-        sprite_group = pygame.sprite.GroupSingle()
+        self.sprite_group = pygame.sprite.GroupSingle()
         sprite = Sprite(path, tile_size)
-        sprite_group.add(sprite)
+        self.sprite_group.add(sprite)
 
         pygame.display.set_caption('Sprite animations')
         self.run()
@@ -51,16 +68,16 @@ class Sprite_animator():
                     pygame.quit()
                     sys.exit()
 
-            self.display_surface.fill('black')
+            self.display_surface.fill('grey')
 
-
+            self.sprite_group.update()
+            self.sprite_group.draw(self.display_surface)
+            
 
             pygame.display.update()
             self.clock.tick(60)
 
-            
-
 
 
 if __name__ == '__main__':
-    animator = Sprite_animator('./tile_folder', 32)
+    animator = Sprite_animator('./tile_folder/idle.png', 32)
